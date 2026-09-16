0c5422dfdfe55adc64cae4c1164d7976cb411c5d
kkkkkiq <sanglardkaique@gmail.com>
2026-09-16 17:09:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,3 +35,30 @@ function sumir() {
     document.getElementById("nao").style.display = "none";
 
 }
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+
+import {
+    getFirestore,
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBCK3GllJATXLb4xXA3UfPVVn0o4bouKHc",
+    authDomain: "tomar-uma-b4eb1.firebaseapp.com",
+    projectId: "tomar-uma-b4eb1",
+    storageBucket: "tomar-uma-b4eb1.firebasestorage.app",
+    messagingSenderId: "637521049777",
+    appId: "1:637521049777:web:285a9ff789e8d9140dca90",
+    measurementId: "G-NH9N7ZQB12"
+};
+
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Conecta ao Firestore
+const db = getFirestore(app);
